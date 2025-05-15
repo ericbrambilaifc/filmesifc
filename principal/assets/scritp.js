1928301923b94566b35e7feea99b5dd33ba135d9
GuiOP11 <deoliveirapadilhag@gmail.com>
2025-05-15 16:30:51 -0300

    velozes

---
+++ principal/assets/scritp.js
@@ -0,0 +1,20 @@
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 5,
+      }
+    }
+  });
+
