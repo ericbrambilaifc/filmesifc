18555bed12d16f6ef16e2e35daf498869b311c86
GuiOP11 <deoliveirapadilhag@gmail.com>
2025-05-19 16:39:27 -0300

    color

--- principal/assets/scritp.js
+++ principal/assets/scritp.js
@@ -20,16 +20,6 @@
   
   
 
-function mostrarDetalhes(titulo, autor, ano, oscars, imagem) {
- 
-  console.log("Abrindo modal para: " + titulo); // Verifica se a função foi chamada
-  document.getElementById('modalTitulo').innerText = titulo;
-  document.getElementById('modalAutor').innerText = "Autor: " + autor;
-  document.getElementById('modalAno').innerText = "Ano de Lançamento: " + ano;
-  document.getElementById('modalOscars').innerText = "Oscars: " + oscars;
-  document.getElementById('modalImagem').src = imagem;
 
-  document.getElementById('modalFilme').style.display = "block";
-}
 
 
--- principal/filmes.php
+++ principal/filmes.php
@@ -47,100 +47,113 @@
           </div>
         </div>
 
-        <div class="swiper-slide card-filme">
-          <img src="partials/velozes.png" alt="VElozes e Furiozos 11" />
-          <div class="info">
-            <h3>Velozes e Furiosos</h3>
-            <p>Autor: Universal</p>
-          </div>
-        </div>
+      
 
       </div>
     </div>
   </section>
 
-  <!-- Modal para exibir detalhes do filme -->
-<div id="modalFilme" class="modal">
-  <div class="modal-content">
-    <span class="close" onclick="fecharModal()">&times;</span>
-    <img id="modalImagem" src="" alt="">
-    <h2 id="modalTitulo"></h2>
-    <p id="modalAutor"></p>
-    <p id="modalAno"></p>
-    <p id="modalOscars"></p>
-    <button id="botaoAssistir">Assistir</button>
-  </div>
-</div>
-
+ 
 
   <?php
   $filmes = [
-      ["titulo" => "Velozes e Furiosos 11", "autor" => "Universal", "ano" => 2025, "oscars" => 3, "imagem" => "partials/velozes.png"],
-      ["titulo" => "Milionário e José Rico", "autor" => "Showlivre","ano"=> 2000, "oscars"=>0, "imagem" => "partials/milionario.jpg"],
-      ["titulo" => "PSG x Inter 2025", "autor" => "Champions League","ano"=> 2025, "oscars"=>"Jogos Ao vivo", "imagem" => "partials/psg.jpg"],
-      ["titulo" => "Chaves", "autor" => "Televisa", "ano"=> 1999, "oscars"=>0, "imagem" => "partials/chaves.jpg"],
-      ["titulo" => "Breaking Bad", "autor" => "AMC","ano"=> 2005, "oscars" =>1, "imagem" => "partials/BreakingBad.jpg"],
-      ["titulo" => "Kick Buttowski", "autor" => "Disney XD","ano"=> 2000, "oscars" =>0, "imagem" => "partials/kick.jpg"],
-      ["titulo" => "NBA Denver Nuggets x Los Angeles", "autor" => "NBA", "ano"=>2025, "oscars"=> "Jogos Ao vivo", "imagem" => "partials/nba.jpg"],
-      ["titulo" => "Lua", "autor" => "Disney","ano"=> 2024,"oscars"=> 0,"imagem" => "partials/lua.jpg"],
+    ["titulo" => "Velozes e Furiosos 11", "autor" => "Universal", "ano" => 2025, "oscars" => 3, "imagem" => "partials/velozes.png"],
+    ["titulo" => "Milionário e José Rico", "autor" => "Showlivre", "ano" => 2000, "oscars" => 0, "imagem" => "partials/milionario.jpg"],
+    ["titulo" => "PSG x Inter 2025", "autor" => "Champions League", "ano" => 2025, "oscars" => "Jogos Ao vivo", "imagem" => "partials/psg.jpg"],
+    ["titulo" => "Chaves", "autor" => "Televisa", "ano" => 1999, "oscars" => 0, "imagem" => "partials/chaves.jpg"],
+    ["titulo" => "Breaking Bad", "autor" => "AMC", "ano" => 2005, "oscars" => 1, "imagem" => "partials/BreakingBad.jpg"],
+    ["titulo" => "Kick Buttowski", "autor" => "Disney XD", "ano" => 2000, "oscars" => 0, "imagem" => "partials/kick.jpg"],
+    ["titulo" => "NBA Denver Nuggets x Los Angeles", "autor" => "NBA", "ano" => 2025, "oscars" => "Jogos Ao vivo", "imagem" => "partials/nba.jpg"],
+    ["titulo" => "Lua", "autor" => "Disney", "ano" => 2024, "oscars" => 0, "imagem" => "partials/lua.jpg"],
   ];
   ?>
 
- <section class="carrossel-netflix">
+  <section class="carrossel-netflix">
     <h2 class="catalogo-titulo">Mais Populares</h2>
     <div class="container-carrossel">
-      <?php 
-      for ($i = 0; $i < count($filmes); $i++) { 
+      <?php
+      for ($i = 0; $i < count($filmes); $i++) {
       ?>
-          <div class="filme-card" onclick="mostrarDetalhes('<?= $filmes[$i]['titulo'] ?>', '<?= $filmes[$i]['autor'] ?>', <?= $filmes[$i]['ano']?>,<?= $filmes[$i]['oscars']?>, '<?= $filmes[$i]['imagem'] ?>')">
-              <img src="<?= $filmes[$i]['imagem'] ?>" alt="<?= $filmes[$i]['titulo'] ?>" />
-              <div class="info">
-                  <h3><?= $filmes[$i]['titulo'] ?></h3>
-                  <p><?= $filmes[$i]['autor'] ?></p>
-              </div>
+        <div class="filme-card" onclick="mostrarDetalhes('<?= $filmes[$i]['titulo'] ?>', '<?= $filmes[$i]['autor'] ?>', <?= $filmes[$i]['ano'] ?>,<?= $filmes[$i]['oscars'] ?>, '<?= $filmes[$i]['imagem'] ?>')">
+          <img src="<?= $filmes[$i]['imagem'] ?>" alt="<?= $filmes[$i]['titulo'] ?>" />
+          <div class="info">
+            <h3><?= $filmes[$i]['titulo'] ?></h3>
+            <p><?= $filmes[$i]['autor'] ?></p>
           </div>
-      <?php 
-      } 
+        </div>
+      <?php
+      }
       ?>
     </div>
-</section>
+  </section>
 
 
- <section class="carrossel-netflix">
+  <section class="carrossel-netflix">
 
     <h2 class="catalogo-titulo">Para você</h2>
-    <div class="container-carrossel">
-  <?php 
+<div class="container-carrossel">
+  <?php
   $filmes2 = [
-      ["titulo" => "The Office", "autor" => "Showlivre","ano"=> 1999,"oscars"=>0, "imagem" => "partials/theoffice.jpg"],
-      ["titulo" => "Os Sete Monstrinhos", "autor" => "Showlivre","ano"=> 1998, "oscars"=>0, "imagem" => "partials/monstrinhos.jpg"],
-    ["titulo" =>"Homem de Ferro 4", "autor" => "Ryan de Oliveira", "ano"=> 2028,"oscars"=> 0,"imagem" => "partials/homem.jpg"],
-     ["titulo" => "PSG x Inter 2025", "autor" => "Champions League","ano"=> 2025, "oscars"=>"Jogos Ao vivo", "imagem" => "partials/psg.jpg"],
-      ["titulo" => "Chaves", "autor" => "Televisa", "ano"=> 1999, "oscars"=>0, "imagem" => "partials/chaves.jpg"],
-      ["titulo" => "Breaking Bad", "autor" => "AMC","ano"=> 2005, "oscars" =>1, "imagem" => "partials/BreakingBad.jpg"],
-      ["titulo" => "Kick Buttowski", "autor" => "Disney XD","ano"=> 2000, "oscars" =>0, "imagem" => "partials/kick.jpg"],
-      ["titulo" => "NBA Denver Nuggets x Los Angeles", "autor" => "NBA", "ano"=>2025, "oscars"=> "Jogos Ao vivo", "imagem" => "partials/nba.jpg"],
-      ["titulo" => "Lua", "autor" => "Disney","ano"=> 2024,"oscars"=> 0,"imagem" => "partials/lua.jpg"],
+    ["titulo" => "The Office", "autor" => "Showlivre", "ano" => 1999, "oscars" => 0, "imagem" => "partials/theoffice.jpg"],
+    ["titulo" => "Os Sete Monstrinhos", "autor" => "Showlivre", "ano" => 1998, "oscars" => 0, "imagem" => "partials/monstrinhos.jpg"],
+    ["titulo" => "Homem de Ferro 4", "autor" => "Ryan de Oliveira", "ano" => 2028, "oscars" => 0, "imagem" => "partials/homem.jpg"],
+   
+    ["titulo" => "Chaves", "autor" => "Televisa", "ano" => 1999, "oscars" => 0, "imagem" => "partials/chaves.jpg"],
+    ["titulo" => "Breaking Bad", "autor" => "AMC", "ano" => 2005, "oscars" => 1, "imagem" => "partials/BreakingBad.jpg"],
+    ["titulo" => "Kick Buttowski", "autor" => "Disney XD", "ano" => 2000, "oscars" => 0, "imagem" => "partials/kick.jpg"],
+    ["titulo" => "NBA Denver Nuggets x Los Angeles", "autor" => "NBA", "ano" => 2025, "oscars" => "Jogos Ao vivo", "imagem" => "partials/nba.jpg"],
+    ["titulo" => "Lua", "autor" => "Disney", "ano" => 2024, "oscars" => 0, "imagem" => "partials/lua.jpg"],
   ];
-  for ($i = 0; $i < count($filmes2); $i++) { 
+  for ($i = 0; $i < count($filmes2); $i++) {
+    $modalId = "staticBackdrop" . $i;
   ?>
-<dialog>
 
-    <div class="filme-card" onclick="mostrarDetalhes('<?= $filmes2[$i]['titulo'] ?>', '<?= $filmes2[$i]['autor'] ?>', <?= $filmes2[$i]['ano'] ?>, <?= $filmes2[$i]['oscars'] ?>, '<?= $filmes2[$i]['imagem'] ?>')">
-        <img src="<?= $filmes2[$i]['imagem'] ?>" alt="<?= $filmes2[$i]['titulo'] ?>" />
-        <div class="info">
-            <h3><?= $filmes2[$i]['titulo'] ?></h3>
-            <p><?= $filmes2[$i]['autor'] ?></p>
-        </div>
+    <div class="filme-card">
+      <img src="<?= $filmes2[$i]['imagem'] ?>" alt="<?= $filmes2[$i]['titulo'] ?>" />
+      <div class="info">
+        <h3><?= $filmes2[$i]['titulo'] ?></h3>
+        <p><?= $filmes2[$i]['autor'] ?></p>
+
+        <!-- Botão que abre o modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#<?= $modalId ?>">
+          Assistir
+        </button>
+
+      
+
+      </div>
+      
     </div>
-<?php 
-} 
-?>
-</dialog>
+  <!-- Modal Bootstrap -->
+        <div class="modal fade" id="<?= $modalId ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="<?= $modalId ?>Label" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="<?= $modalId ?>Label"><?= $filmes2[$i]['titulo'] ?></h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+              </div>
+              <div class="modal-body">
+                <img src="<?= $filmes2[$i]['imagem'] ?>" alt="<?= $filmes2[$i]['titulo'] ?>" width="200" class="img-fluid mb-3" />
+                <p class=""><strong >Autor:</strong> <?= $filmes2[$i]['autor'] ?></p>
+                <p><strong>Ano:</strong> <?= $filmes2[$i]['ano'] ?></p>
+                <p><strong>Oscars:</strong> <?= $filmes2[$i]['oscars'] ?></p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+  <?php
+  }
+  ?>
 </div>
-</section>
 
+    
+  </section>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 
 </html>
