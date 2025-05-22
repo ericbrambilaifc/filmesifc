799e39aa323ced5361118e30ae63ca5ca50a74a8
ericbrambilaifc <ericbrambilaifc@gmail.com>
2025-05-22 16:32:12 -0300

    brambila

--- principal/filmes.php
+++ principal/filmes.php
@@ -26,44 +26,44 @@
 <body>
 
   <?php
-
+  
   $filmes_populares = [
-    [
+   [
       "titulo" => "Clube da Luta",
-      "autor" => "David Fincher",
-      "ano" => 1999,
-      "oscars" => 0,
+      "autor" => "David Fincher",  
+      "ano" => 1999, 
+      "oscars" => 0, 
       "imagem" => "partials/clubedaluta.jpg",
       "link" => "https://www.netflix.com/title/26004747"
     ],
     [
-      "titulo" => "Homem Aranha",
-      "autor" => "Bob Persichetti",
-      "ano" => 2018,
-      "oscars" => 0,
+      "titulo" => "Homem Aranha", 
+      "autor" => "Bob Persichetti", 
+      "ano" => 2018, 
+      "oscars" => 0, 
       "imagem" => "partials/image.png",
       "link" => "https://www.netflix.com/title/81002747"
     ],
     [
-      "titulo" => "Milionario e Jose Rico",
-      "autor" => "Jose da Silva",
-      "ano" => 1988,
-      "oscars" => 0,
+      "titulo" => "Milionario e Jose Rico", 
+      "autor" => "Jose da Silva", 
+      "ano" => 1988, 
+      "oscars" => 0, 
       "imagem" => "partials/milionario.jpg",
       "link" => "https://www.primevideo.com/"
     ],
     [
       "titulo" => "Tropa de Elite",
-      "autor" => "José Padilha",
+      "autor" => "José Padilha", 
       "ano" => 2007,
       "oscars" => 0,
       "imagem" => "partials/utils/tropa1.jpg",
       "link" => "https://www.netflix.com/title/70136120"
-
+    
     ],
     [
       "titulo" => "tropa de elite 2",
-      "autor" => "José Padilha",
+      "autor" => "José Padilha", 
       "ano" => 2010,
       "oscars" => 0,
       "imagem" => "partials/utils/tropa2.jpg",
@@ -71,27 +71,27 @@
     ],
     [
       "titulo" => "Cidade de Deus",
-      "autor" => "Fernando Meirelles",
-      "ano" => 2002,
-      "oscars" => 0,
+      "autor" => "Fernando Meirelles", 
+      "ano" => 2002,  
+      "oscars" => 0, 
       "imagem" => "partials/utils/cidade.jpg",
       "link" => "https://www.netflix.com/title/80191024"
     ],
     [
       "titulo" => "Os suburbanos",
-      "autor" => "Luciano Sabino",
+      "autor" => "Luciano Sabino", 
       "ano" => 2022,
-
-      "oscars" => 0,
+       
+      "oscars" => 0, 
       "imagem" => "partials/utils/suburbanos.jpg",
       "link" => "https://www.disneyplus.com/"
     ],
     [
       "titulo" => "velozes e furiosos",
-      "autor" => "Rob Cohen",
+      "autor" => "Rob Cohen", 
       "ano" => 2001,
       "oscars" => 0,
-
+      
       "imagem" => "partials/utils/furi.jpg",
       "link" => "https://www.netflix.com/title/81076856"
     ],
@@ -129,82 +129,82 @@
     ]
   ];
   $filmes_para_voce = [
-    [
-      "titulo" => "Batman",
+ [
+      "titulo" => "Batman", 
       "autor" => "Matt Reeves",
       "ano" => 2022,
-      "oscars" => 0,
+      "oscars" => 0, 
       "imagem" => "partials/utils/bat.jpg",
       "link" => "https://www.netflix.com/title/70136120"
     ],
     [
-      "titulo" => "Coringa",
+      "titulo" => "Coringa", 
       "autor" => "Todd Phillips",
       "ano" => 2019,
-      "oscars" => 2,
+      "oscars" => 2, 
       "imagem" => "partials/utils/coringa.jpg",
       "link" => "https://www.netflix.com/"
     ],
     [
       "titulo" => "Homem Aranha 3",
-      "autor" => "Sam Raimi",
-      "ano" => 2007,
-
-      "oscars" => 0,
+      "autor" => "Sam Raimi", 
+      "ano" => 2007, 
+       
+      "oscars" => 0, 
       "imagem" => "partials/utils/homemaranha.jpg",
       "link" => "https://www.disneyplus.com/"
     ],
     [
       "titulo" => "Pantera Negra",
-      "autor" => "Ryan Coogler",
+      "autor" => "Ryan Coogler", 
       "ano" => 2018,
       "oscars" => 3,
-
+      
       "imagem" => "partials/utils/pantera.jpg",
       "link" => "https://www.espn.com.br/"
     ],
     [
       "titulo" => "Enola Holmes",
-      "autor" => "Harry Bradbeer",
+      "autor" => "Harry Bradbeer", 
       "ano" => 2020,
-
-      "oscars" => 0,
+      
+      "oscars" => 0, 
       "imagem" => "partials/utils/enola.jpg",
       "link" => "https://www.primevideo.com/detail/Chaves/0JGKTUD4UOLES3FIEFMC2RRKJY"
     ],
     [
       "titulo" => "Ainda estou aqui",
-      "autor" => "David Leitch",
+      "autor" => "David Leitch", 
       "ano" => 2024,
-      "oscars" => 1,
-      "imagem" => "partials/utils/ainda.jpg",
+      "oscars" => 1, 
+      "imagem" => "partials/utils/ainda.jpg", 
       "link" => "https://www.netflix.com/"
-
+ 
     ],
     [
-      "titulo" => "Grenn Book",
+      "titulo" => "Grenn Book", 
       "autor" => "Peter Farrelly",
       "ano" => 1962,
-      "oscars" => 0,
+      "oscars" => 0, 
       "imagem" => "partials/utils/green.jpg",
       "link" => "https://www.netflix.com/"
-
+     
     ],
     [
       "titulo" => "It a Coisa",
-      "autor" => "Andy Muschietti",
+      "autor" => "Andy Muschietti", 
       "ano" => 2017,
       "oscars" => 0,
       "imagem" => "partials/utils/it.jpg",
-
+      
       "link" => "https://www.netflix.com/"
-
+      
     ],
     [
       "titulo" => "O caixão",
       "autor" => "Gustavo Bonafé",
       "ano" => 2022,
-
+      
       "oscars" => 0,
       "imagem" => "partials/utils/caixao.jpg",
       "link" => "https://www.netflix.com/"
@@ -213,7 +213,7 @@
       "titulo" => "Corra",
       "autor" => "Jordan Peele",
       "ano" => 2017,
-
+      
       "oscars" => 0,
       "imagem" => "partials/utils/corra.jpg",
       "link" => "https://www.netflix.com/"
@@ -222,8 +222,8 @@
       "titulo" => "Rio",
       "autor" => "Carlos Saldanha",
       "ano" => 2011,
-
-
+      
+      
       "oscars" => 0,
       "imagem" => "partials/utils/rio.jpg",
       "link" => "https://www.netflix.com/"
@@ -267,44 +267,44 @@
       "oscars" => 2,
       "imagem" => "partials/utils/rei.jpg",
       "link" => "https://www.netflix.com/"
-
+   
     ],
-    [
+     [
       "titulo" => "Clube da Luta",
-      "autor" => "David Fincher",
-      "ano" => 1999,
-      "oscars" => 0,
+      "autor" => "David Fincher",  
+      "ano" => 1999, 
+      "oscars" => 0, 
       "imagem" => "partials/clubedaluta.jpg",
       "link" => "https://www.netflix.com/title/26004747"
     ],
     [
-      "titulo" => "Homem Aranha",
-      "autor" => "Bob Persichetti",
-      "ano" => 2018,
-      "oscars" => 0,
+      "titulo" => "Homem Aranha", 
+      "autor" => "Bob Persichetti", 
+      "ano" => 2018, 
+      "oscars" => 0, 
       "imagem" => "partials/image.png",
       "link" => "https://www.netflix.com/title/81002747"
     ],
     [
-      "titulo" => "Milionario e Jose Rico",
-      "autor" => "Jose da Silva",
-      "ano" => 1988,
-      "oscars" => 0,
+      "titulo" => "Milionario e Jose Rico", 
+      "autor" => "Jose da Silva", 
+      "ano" => 1988, 
+      "oscars" => 0, 
       "imagem" => "partials/milionario.jpg",
       "link" => "https://www.primevideo.com/"
     ],
     [
       "titulo" => "Tropa de Elite",
-      "autor" => "José Padilha",
+      "autor" => "José Padilha", 
       "ano" => 2007,
       "oscars" => 0,
       "imagem" => "partials/utils/tropa1.jpg",
       "link" => "https://www.netflix.com/title/70136120"
-
+    
     ],
     [
       "titulo" => "tropa de elite 2",
-      "autor" => "José Padilha",
+      "autor" => "José Padilha", 
       "ano" => 2010,
       "oscars" => 0,
       "imagem" => "partials/utils/tropa2.jpg",
@@ -312,27 +312,27 @@
     ],
     [
       "titulo" => "Cidade de Deus",
-      "autor" => "Fernando Meirelles",
-      "ano" => 2002,
-      "oscars" => 0,
+      "autor" => "Fernando Meirelles", 
+      "ano" => 2002,  
+      "oscars" => 0, 
       "imagem" => "partials/utils/cidade.jpg",
       "link" => "https://www.netflix.com/title/80191024"
     ],
     [
       "titulo" => "Os suburbanos",
-      "autor" => "Luciano Sabino",
+      "autor" => "Luciano Sabino", 
       "ano" => 2022,
-
-      "oscars" => 0,
+       
+      "oscars" => 0, 
       "imagem" => "partials/utils/suburbanos.jpg",
       "link" => "https://www.disneyplus.com/"
     ],
     [
       "titulo" => "velozes e furiosos",
-      "autor" => "Rob Cohen",
+      "autor" => "Rob Cohen", 
       "ano" => 2001,
       "oscars" => 0,
-
+      
       "imagem" => "partials/utils/furi.jpg",
       "link" => "https://www.netflix.com/title/81076856"
     ],
@@ -368,66 +368,66 @@
       "imagem" => "partials/utils/vingadores.jpg",
       "link" => "https://www.disneyplus.com/"
     ],
-    [
+     [
       "titulo" => "Homem Aranha 3",
-      "autor" => "Sam Raimi",
-      "ano" => 2007,
-
-      "oscars" => 0,
+      "autor" => "Sam Raimi", 
+      "ano" => 2007, 
+       
+      "oscars" => 0, 
       "imagem" => "partials/utils/homemaranha.jpg",
       "link" => "https://www.disneyplus.com/"
     ],
     [
       "titulo" => "Pantera Negra",
-      "autor" => "Ryan Coogler",
+      "autor" => "Ryan Coogler", 
       "ano" => 2018,
       "oscars" => 3,
-
+      
       "imagem" => "partials/utils/pantera.jpg",
       "link" => "https://www.espn.com.br/"
     ],
     [
       "titulo" => "Enola Holmes",
-      "autor" => "Harry Bradbeer",
+      "autor" => "Harry Bradbeer", 
       "ano" => 2020,
-
-      "oscars" => 0,
+      
+      "oscars" => 0, 
       "imagem" => "partials/utils/enola.jpg",
       "link" => "https://www.primevideo.com/detail/Chaves/0JGKTUD4UOLES3FIEFMC2RRKJY"
     ],
     [
       "titulo" => "Ainda estou aqui",
-      "autor" => "David Leitch",
+      "autor" => "David Leitch", 
       "ano" => 2024,
-      "oscars" => 1,
-      "imagem" => "partials/utils/ainda.jpg",
+      "oscars" => 1, 
+      "imagem" => "partials/utils/ainda.jpg", 
       "link" => "https://www.netflix.com/"
-
+ 
     ],
     [
-      "titulo" => "Grenn Book",
+      "titulo" => "Grenn Book", 
       "autor" => "Peter Farrelly",
       "ano" => 1962,
-      "oscars" => 0,
+      "oscars" => 0, 
       "imagem" => "partials/utils/green.jpg",
       "link" => "https://www.netflix.com/"
-
+     
     ],
     [
       "titulo" => "It a Coisa",
-      "autor" => "Andy Muschietti",
+      "autor" => "Andy Muschietti", 
       "ano" => 2017,
       "oscars" => 0,
       "imagem" => "partials/utils/it.jpg",
-
+      
       "link" => "https://www.netflix.com/"
-
+      
     ],
     [
       "titulo" => "O caixão",
       "autor" => "Gustavo Bonafé",
       "ano" => 2022,
-
+      
       "oscars" => 0,
       "imagem" => "partials/utils/caixao.jpg",
       "link" => "https://www.netflix.com/"
@@ -436,7 +436,7 @@
       "titulo" => "Corra",
       "autor" => "Jordan Peele",
       "ano" => 2017,
-
+      
       "oscars" => 0,
       "imagem" => "partials/utils/corra.jpg",
       "link" => "https://www.netflix.com/"
@@ -445,8 +445,8 @@
       "titulo" => "Rio",
       "autor" => "Carlos Saldanha",
       "ano" => 2011,
-
-
+      
+      
       "oscars" => 0,
       "imagem" => "partials/utils/rio.jpg",
       "link" => "https://www.netflix.com/"
@@ -488,67 +488,35 @@
   </header>
 
   <section id="catalogo-section">
-    <h2 id="catalogo-titulo">Filme em Destaque</h2>
-    <div class="swiper mySwiper">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide card-filme"
-          data-titulo="Homem Aranha"
-          data-autor="Bob Persichetti"
-          data-ano="2018"
-          data-oscars="0"
-          data-imagem="partials/image.png">
-          <img src="partials/image.png" alt="Homem Aranha" />
-          <div class="info">
-            <h3>Homem Aranha</h3>
-            <p>Autor: Bob Persichetti</p>
-          </div>
-          <a target="_blank" href="https://www.netflix.com/title/81002747" class="play-icon">
-            <i class="bi bi-play-circle-fill" style="color: white; font-size: 2rem; margin-left: 6px;"></i>
-          </a>
-
-          <button type="button" class="info-icon btn btn-link text-white p-0" style="background: none; border: none;">
-            <i class="bi bi-exclamation-circle"
-              style="color: white; font-size: 2rem; margin-left: 6px; cursor:pointer;"
-              onclick="mostrarDetalhes(this)"></i>
-          </button>
+  <h2 id="catalogo-titulo">Filme em Destaque</h2>
+  <div class="swiper mySwiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide card-filme" 
+           data-titulo="Homem Aranha"
+           data-autor="Bob Persichetti"
+           data-ano="2018"
+           data-oscars="0"
+           data-imagem="partials/image.png">
+        <img src="partials/image.png" alt="Homem Aranha" />
+        <div class="info">
+          <h3>Homem Aranha</h3>
+          <p>Autor: Bob Persichetti</p>
         </div>
+        <a target="_blank" href="https://www.netflix.com/title/81002747" class="play-icon">
+          <i class="bi bi-play-circle-fill" style="color: white; font-size: 2rem; margin-left: 6px;"></i>
+        </a>
+
+        <button type="button" class="info-icon btn btn-link text-white p-0" style="background: none; border: none;">
+          <i class="bi bi-exclamation-circle" 
+             style="color: white; font-size: 2rem; margin-left: 6px; cursor:pointer;" 
+             onclick="mostrarDetalhes(this)"></i>
+        </button>
       </div>
     </div>
-    <h1>Filmes por Gênero</h1>
-    <form method="get">
-      <label for="genero">Escolha um gênero:</label>
-      <select name="genero" id="genero" onchange="this.form.submit()">
-        <option value="">-- Todos --</option>
-        <?php foreach ($generos as $genero): ?>
-          <option value="<?= htmlspecialchars($genero) ?>" <?= ($genero === $genero_selecionado) ? 'selected' : '' ?>>
-            <?= htmlspecialchars($genero) ?>
-          </option>
-        <?php endforeach; ?>
-      </select>
-    </form>
+  </div>
 
-    <?php if (!empty($genero_selecionado)): ?>
-      <h2>Séries do gênero "<?= htmlspecialchars($genero_selecionado) ?>"</h2>
-      <?php if (!empty($series_filtradas)): ?>
-        <?php foreach ($series_filtradas as $serie): ?>
-          <div class="serie">
-            <img src="<?= htmlspecialchars($serie['imagem']) ?>" alt="<?= htmlspecialchars($serie['titulo']) ?>">
-            <div>
-              <h3><?= htmlspecialchars($serie['titulo']) ?></h3>
-              <p><strong>Autor:</strong> <?= htmlspecialchars($serie['autor']) ?></p>
-              <p><strong>Ano:</strong> <?= htmlspecialchars($serie['ano']) ?></p>
-              <p><a href="<?= htmlspecialchars($serie['link']) ?>">Ver mais</a></p>
-            </div>
-          </div>
-        <?php endforeach; ?>
-      <?php else: ?>
-        <p>Não há séries para este gênero.</p>
-      <?php endif; ?>
-    <?php else: ?>
-      <p>Selecione um gênero para ver as séries correspondentes.</p>
-    <?php endif; ?>
-
-  </section>
+  
+</section>
 
 
   <!-- modal  -->
@@ -615,38 +583,40 @@
 
   <!-- Rodapé -->
 
-  <footer>
-    <div class="footer-container">
-      <div class="footer-links">
-        <ul>
-          <li><a href="sobre.php">Sobre Nós</a></li>
-          <li><a href="contato.php">Contato</a></li>
-        </ul>
-      </div>
-      <div class="footer-info">
-        <p>&copy; 2025 Filmes+. Todos os direitos reservados.</p>
-      </div>
+<footer>
+  <div class="footer-container">
+    <div class="footer-links">
+      <ul>
+        <li><a href="sobre.php">Sobre Nós</a></li>
+        <li><a href="contato.php">Contato</a></li>
+      </ul>
     </div>
-  </footer>
+    <div class="footer-info">
+      <p>&copy; 2025 Filmes+. Todos os direitos reservados.</p>
+    </div>
+  </div>
+
+</footer>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
   <script>
-    function mostrarDetalhes(btn) {
-      const card = btn.closest('[data-titulo]');
-      if (!card) return;
+   function mostrarDetalhes(btn) {
+  const card = btn.closest('[data-titulo]');
+  if (!card) return;
 
-      document.getElementById('infoModalLabel').textContent = card.dataset.titulo;
-      document.getElementById('modalImagem').src = card.dataset.imagem;
-      document.getElementById('modalImagem').alt = card.dataset.titulo;
-      document.getElementById('modalAutor').textContent = card.dataset.autor;
-      document.getElementById('modalAno').textContent = card.dataset.ano;
-      document.getElementById('modalOscars').textContent = card.dataset.oscars;
+  document.getElementById('infoModalLabel').textContent = card.dataset.titulo;
+  document.getElementById('modalImagem').src = card.dataset.imagem;
+  document.getElementById('modalImagem').alt = card.dataset.titulo;
+  document.getElementById('modalAutor').textContent = card.dataset.autor;
+  document.getElementById('modalAno').textContent = card.dataset.ano;
+  document.getElementById('modalOscars').textContent = card.dataset.oscars;
 
-      const modal = new bootstrap.Modal(document.getElementById('infoModal'));
-      modal.show();
-    }
+  const modal = new bootstrap.Modal(document.getElementById('infoModal'));
+  modal.show();
+}
+
   </script>
-
+  
 
 
 </body>
